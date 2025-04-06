477a32fd4d1897feef7fa02a76628376c0ea4621
Vikash kumar kharwar <amit.sbz1596@gmail.com.com>
2025-04-06 21:08:22 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "wgrvuokkrcmqxvozplgv.supabase.co",
+            "oeclfpsoxfsvisgwklvx.supabase.co"
+        ],
+    },
+};
+
+module.exports = nextConfig;
